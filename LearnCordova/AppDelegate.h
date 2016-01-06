5fbdccbad53288f2d1defabd752fa09cc11a973e
tmyk104117 <tmyk104117@126.com>
2016-01-06 18:55:56 +0800

    create project

---
+++ LearnCordova/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnCordova
+//
+//  Created by szhang on 06/01/2016.
+//  Copyright © 2016 szhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnCordova/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnCordova
+//
+//  Created by szhang on 06/01/2016.
+//  Copyright © 2016 szhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
